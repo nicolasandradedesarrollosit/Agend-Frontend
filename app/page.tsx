710327c2aadb483d2de565f77a091e34d1992225
nicolasandradedesarrollosit <nicolasandradedesarrollos@gmail.com>
2025-11-04 12:00:46 -0300

    📱 feat: scroll snap fixes v2

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
 import Hero from "@/app/_components/Hero";
 import FirstSection from "@/app/_components/FirstSection";
 
+import {
+  useState,
+  useEffect
+} from "react";
+
 function Home() {
+  const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 768);
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    
   return (
-    <div className="h-screen overflow-y-scroll snap-y snap-mandatory">
+    <div className={`h-screen overflow-y-scroll ${isMobile ? "snap-none" : "snap-y snap-mandatory"}`}>
       <Hero />
       <FirstSection />
     </div>
